38e3e7011e09ccb055b7ab4ed783d9d0b9680319
Kashif13 <kashifjamal13@gmail.com>
2025-02-24 18:56:26 -0500

    Task 1: counter with increment and decrement functionality

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Counter from './Counter';
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        {/* Task 1: Counter Component */}
+        <Counter />
         <a
           className="App-link"
           href="https://reactjs.org"
